354de6bc395af77ef705090d01c9eb1b81fb3710
Nicolas <nmpandafish6@gmail.com>
2016-06-28 22:11:00 -0700

    Added command args for opening raw files.

--- src/programbuilder/TGG_Master.java
+++ src/programbuilder/TGG_Master.java
@@ -1,7 +1,12 @@
 package programbuilder;
 
-import programbuilder.components.GUI_Frame;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import programbuilder.components.GUI_Frame;
+import programbuilder.resources.Resources;
+import terraingenerator.TGG_FileOperations;
 
 /**
  * Terrain Gen Gui Master File
@@ -14,11 +19,19 @@ public class TGG_Master {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         GUI_Frame frame = GUI_Frame.getInstance();
         frame.setTitle("Fortress Games TGG");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.pack();
+        System.out.println("HEY");
+        if(args.length > 0){
+            System.out.println(args[0]);
+                        Resources.dataArrayList.set(0, TGG_FileOperations.readBinary(args[0]));
+                        GUI_Frame.updateImage(Resources.dataArrayList.get(0));
+                        GUI_Frame.updateStats(Resources.dataArrayList.get(0));
+        }
     }
     
 }
